bd4759a5543d0fdd39ec852b0a4a0185982a71bc
João Gomes <devjoaoantoniogomes@gmail.com>
2022-03-25 18:36:24 -0300

    Puts, chomp, to_i, addition, print

---
+++ sum.rb
@@ -0,0 +1,9 @@
+print "Digite o primeiro número inteiro: "
+number1 = gets.chomp.to_i
+
+print "Digite o segundo número inteiro: "
+number2 = gets.chomp.to_i
+
+number1 += number2
+
+puts "O resultado da adição entre os dois números é #{number1}"
